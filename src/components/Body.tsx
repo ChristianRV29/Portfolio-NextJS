f24e72a2efe8c018930d7a81f34f6e51fba3a239
ChristianRV29 <kmylorv@gmail.com>
2022-02-22 19:06:44 -0500

    Projects included within an another section

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,57 +1,77 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 import { SkillProps } from '~src/@types/skill'
 import { experiences } from '~src/constants/experience'
+import { projects } from '~src/constants/projects'
 import { skills } from '~src/constants/skills'
 import Experience from './Experience'
 import Skill from './Skill'
 
 const Body = () => {
   return (
-    <div className={'row py-3'}>
-      <div className={'col-md-4'}>
-        <div className={'card bg-muted'}>
-          <div className={'card-body'}>
-            <h3 className={'text-center'}>Skills</h3>
-            {(skills || []).map((skill: SkillProps, index: number) => (
-              <Skill {...skill} key={index} />
-            ))}
+    <Fragment>
+      <section className={'row py-3'}>
+        <div className={'col-md-4'}>
+          <div className={'card bg-muted'}>
+            <div className={'card-body'}>
+              <h3 className={'text-center'}>Skills</h3>
+              {(skills || []).map((skill: SkillProps, index: number) => (
+                <Skill {...skill} key={index} />
+              ))}
+            </div>
           </div>
         </div>
-      </div>
-      <div className={'col-md-8'}>
-        <article className={'card bg-muted'}>
-          <h3 className={'text-center'}>Experience</h3>
-          <ul>
-            {(experiences || []).map((exp, index: number) => (
-              <Experience {...exp} key={index} />
-            ))}
-          </ul>
-        </article>
-      </div>
-      {/** Projects section  */}
-      <div className={'row py-2'}>
-        <div className={"col-md-12"}>
-          <div className={"card card-body bg-primary"}>
-            <div className={"row"}>
-              <div className={'col-md-12'}>
-                  <h1 className={'text-center text-light'}>Portfolio</h1>
-              </div>
-              <div className={'col-md-4'}>
-                <div className={'card bg-secondary'}>
-                  <img src={'https://media.istockphoto.com/photos/project-management-with-icons-about-planning-tasks-and-milestones-on-picture-id1135541613?k=20&m=1135541613&s=612x612&w=0&h=rHctItxe3Vi5AGnLfm-Qa_PARqNXaxhlc3TbFYFe3nI='} alt="project-image.png" />
-                  <div className="card-body">
-                    <h3>Title</h3>
-                    <p>Description</p>
-                    <a href={'#'}>Know more</a>
+        <div className={'col-md-8'}>
+          <article className={'card bg-muted'}>
+            <h3 className={'text-center'}>Experience</h3>
+            <ul>
+              {(experiences || []).map((exp, index: number) => (
+                <Experience {...exp} key={index} />
+              ))}
+            </ul>
+          </article>
+        </div>
+      </section>
+      <section>
+        <div className="row">
+          <div className="col-md-12">
+            <div className="card card-body bg-dark">
+              <div className="row">
+                <div className="col-md-12 my-2">
+                  <h1 className="text-center">Portfolio</h1>
+                </div>
+                {projects.map(({ name, description, image }, index) => (
+                  <div className="col-md-4 p-2" key={index}>
+                    <div className="card h-100 border-white bg-secondary">
+                      <div className="overflow">
+                        <img
+                          src={`${image}`}
+                          alt=""
+                          className="card-img-top"
+                        />
+                      </div>
+                      <div className="card-body">
+                        <h3>{name}</h3>
+                        <p>{description}</p>
+                        <a href="#!">Know More</a>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+
+                <div className="col-md-12 mt-4">
+                  <div className="text-center">
+                    {/* <Link href="/portfolio">
+                    <a className="btn btn-outline-light">More Projects</a>
+                  </Link> */}
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
+      </section>
+    </Fragment>
   )
 }
 
---
+++ src/constants/projects.ts
@@ -0,0 +1,45 @@
+export const projects = [
+  {
+    name: 'Awesome project 1',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum auctor rutrum lectus. Quisque cursus tempus commodo.',
+    image:
+      'https://media.istockphoto.com/photos/project-management-with-icons-about-planning-tasks-and-milestones-on-picture-id1135541613?k=20&m=1135541613&s=612x612&w=0&h=rHctItxe3Vi5AGnLfm-Qa_PARqNXaxhlc3TbFYFe3nI=',
+  },
+  {
+    name: 'Awesome project 2',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum auctor rutrum lectus. Quisque cursus tempus commodo.',
+    image:
+      'https://media.istockphoto.com/photos/project-management-with-icons-about-planning-tasks-and-milestones-on-picture-id1135541613?k=20&m=1135541613&s=612x612&w=0&h=rHctItxe3Vi5AGnLfm-Qa_PARqNXaxhlc3TbFYFe3nI=',
+  },
+  {
+    name: 'Awesome project 3',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum auctor rutrum lectus. Quisque cursus tempus commodo.',
+    image:
+      'https://media.istockphoto.com/photos/project-management-with-icons-about-planning-tasks-and-milestones-on-picture-id1135541613?k=20&m=1135541613&s=612x612&w=0&h=rHctItxe3Vi5AGnLfm-Qa_PARqNXaxhlc3TbFYFe3nI=',
+  },
+  {
+    name: 'Awesome project 4',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum auctor rutrum lectus. Quisque cursus tempus commodo.',
+    image:
+      'https://media.istockphoto.com/photos/project-management-with-icons-about-planning-tasks-and-milestones-on-picture-id1135541613?k=20&m=1135541613&s=612x612&w=0&h=rHctItxe3Vi5AGnLfm-Qa_PARqNXaxhlc3TbFYFe3nI=',
+  },
+  {
+    name: 'Awesome project 5',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum auctor rutrum lectus. Quisque cursus tempus commodo.',
+    image:
+      'https://media.istockphoto.com/photos/project-management-with-icons-about-planning-tasks-and-milestones-on-picture-id1135541613?k=20&m=1135541613&s=612x612&w=0&h=rHctItxe3Vi5AGnLfm-Qa_PARqNXaxhlc3TbFYFe3nI=',
+  },
+  {
+    name: 'Awesome project 6',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum auctor rutrum lectus. Quisque cursus tempus commodo.',
+    image:
+      'https://media.istockphoto.com/photos/project-management-with-icons-about-planning-tasks-and-milestones-on-picture-id1135541613?k=20&m=1135541613&s=612x612&w=0&h=rHctItxe3Vi5AGnLfm-Qa_PARqNXaxhlc3TbFYFe3nI=',
+  },
+  
+]
