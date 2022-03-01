bff369cd73a258727659f34b4227790639b95540
ChristianRV29 <kmylorv@gmail.com>
2022-03-01 14:51:31 -0500

    Github profile added to Github page

--- pages/github.tsx
+++ pages/github.tsx
@@ -1,31 +1,45 @@
 import type { GetServerSidePropsContext, NextPage } from 'next'
+import Image from 'next/image'
 
-import { GithubResponse } from '~src/@types/github';
-import Layout from '~src/components/Layout';
+import { GithubResponse } from '~src/@types/github'
+import Layout from '~src/components/Layout'
 
 interface GithubPageProps {
-  user: GithubPageProps | undefined;
+  user: GithubResponse | undefined
 }
 
 const Github: NextPage<GithubPageProps> = ({ user }) => {
-
+  console.log(user?.avatar_url)
   return (
     <Layout>
-        <div>
-            <h1>Github page</h1>
+      <div className={'row'}>
+        <div className={'col-md-4 offset-md-4'}>
+          <div className={'card card-body text-center'}>
+            <h1>{user!.name}</h1>
+            <Image
+              loader={() => user!.avatar_url}
+              src={user!.avatar_url}
+              width={250}
+              height={250}
+              alt={"Github profile's image"}
+            />
+            <p>{user!.bio}</p>
+            <a href={user!.html_url} className={'btn btn-outline-secondary'} > Go to github profile</a>
+          </div>
         </div>
+      </div>
     </Layout>
-  );
+  )
 }
 
-export async function getServerSideProps (context: GetServerSidePropsContext) {
+export async function getServerSideProps(context: GetServerSidePropsContext) {
   const res = await fetch('https://api.github.com/users/ChristianRV29')
-  const data: GithubResponse = await res.json();
+  const data: GithubResponse = await res.json()
 
   return {
     props: {
       user: data,
-    }
+    },
   }
 }
 
